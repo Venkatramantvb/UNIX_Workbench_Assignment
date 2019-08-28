f7ad159cd8105d1d470e2ae77c59c4d3a64bd2f0
Venkatraman T <venkattvb@gmail.com>
2019-08-28 22:16:40 +0530

    Enhanced the script so that it has function and handling exception as only valid integer it should process

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
+function getting_and_checking_input_value {
+while true
+do
+ echo "Guess the number of files present in the current directory :"
+ read guess_number_of_files
+ export guess_number_of_files=$guess_number_of_files
+ if [[ ! $guess_number_of_files =~ ^[0-9]+$ ]]
+ then
+  echo "Enter a valid number"
+  continue
+ else
+  break
+ fi
+done
+}
 
-actual_number_of_files=$(ls | wc -l)
+function getting_actual_files_list {
+export actual_number_of_files=$(ls | wc -l)
+export try=1
+getting_and_checking_input_value
+}
 
-try=1
-#read -p "Guess the number of files present in the directory : " guess_number_of_files
+getting_actual_files_list
 
 while true
-#while [[ $guess_number_of_files -ge 0 ]]
 do
- #  if [ $try != 1 ]
- #  then
-   read -p "Guess the number of files present in the directory : " guess_number_of_files
- # read guess_number_of_files
+   if [ $try -eq 0 ]
+   then
+    getting_and_checking_input_value
+   fi   
    if [ $guess_number_of_files -ne $guess_number_of_files 2>/dev/null ]
    then
     continue
    fi
-
-  # fi
-
    if [ $guess_number_of_files == $actual_number_of_files ]
    then
+    echo "Congratulations your guess is right!!!"
     exit
+   
    elif [ $guess_number_of_files -gt $actual_number_of_files ]
-    then
-     echo "guess_number_of_files is higher than actual_number_of_files"    
-     echo " "
+   then
+    echo "Your guess is higher than the actual number of files in the present working directory"
+    echo " "
    else
-    echo "guess_number_of_files is lower than actual_number_of_files"
+    echo "Your guess is lower than the actual number of files in the present working directory"
     echo " "
    fi
-
    try=0
-
 done
